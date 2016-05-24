33409c32f4d68791c487f9158fa92b0dfde77faf
Akshar Dave <akshar314@gmail.com>
2016-05-24 14:55:35 +0530

    Add server.js & body-parser

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 var Sequelize = require('Sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',
-    'storage': __dirname +'/data/foodinventory.sqlite'
+    'storage': __dirname + '/data/foodinventory.sqlite'
 });
 
 var db = {};
 
-var menu = sequelize.import(__dirname +'/models/menu.js');
-db.menu = menu;
+db.menu = sequelize.import(__dirname +'/models/menu.js');
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 var PORT = process.env.PORT || 3000;
 var db = require('./db.js')
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
+app.use(bodyParser.json());
 
-db.sequelize.sync().then(function() {
+app.get('/', function(request, response) {
+    response.send("GeekFresh.com");
+});
+
+app.post('/menu', function(request, response) {
+
+    var body = _.pick(request.body,'name','description','quantity','ingredients','category','cost','image','location');
+   // var body = request.body;
+    db.menu.create(body).then(function(menu){
+    	response.json(menu.toJSON());
+    },function(e){
+    	console.log(e);
+    });
+
+
+});
+
+db.sequelize.sync({ force: true }).then(function() {
 
     app.listen(PORT, function() {
 
         console.log("listening at port" + PORT);
-        
+
     });
 
 });
