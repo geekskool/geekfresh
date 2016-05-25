a7304d7da0b58eaf865f59af8a62bf868ea07e54
Akshar Dave <akshar314@gmail.com>
2016-05-25 16:45:38 +0530

    Add addMenu functionality & bootstrap

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname +'/public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('view engine', 'ejs')
 
 app.get('/', function(request, response) {
     console.log("at index");
@@ -23,12 +25,42 @@ app.get('/all', function(request, response) {
 
 });
 
-app.post('/menu', function(request, response) {
+app.get('/admin',function(request,response){
 
-    var body = _.pick(request.body, 'name', 'description', 'quantity', 'ingredients', 'category', 'cost', 'image', 'location');
+    response.sendFile(__dirname+'/public/admin.html');
+
+
+});
+
+app.post('/admin', function(request, response) {
+    
+    
+    // var body = _.pick(request.body, 'name', 'description', 'quantity', 'ingredients', 'category', 'cost', 'image', 'location');
     // var body = request.body;
+
+var name = request.body.name;
+var description = request.body.description;
+var quantity = request.body.quantity;
+var ingredients = request.body.ingredients;
+var category = request.body.category;
+var cost = request.body.cost;
+var image = request.body.image;
+var location = request.body.location;
+
+var body ={
+"name":name,
+"description":description,
+"quantity":quantity,
+"ingredients":ingredients,
+"category":category,
+"cost":cost,
+"image":image,
+"location":location
+
+}
     db.menu.create(body).then(function(menu) {
         response.json(menu.toJSON());
+       // response.send("name"+body.name);
     }, function(e) {
         console.log(e);
     });
