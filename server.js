0fc9393a1ba0a337677157cc8542bec8a714b456
adityabhushan <adityabhushandwivedi@gmail.com>
2016-05-27 11:10:22 +0530

    add GET and POST for admin routes

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('view engine', 'ejs')
 
 app.get('/', function(request, response) {
     console.log("at index");
@@ -14,34 +16,73 @@ app.get('/', function(request, response) {
 
 app.get('/all', function(request, response) {
 
-	db.menu.findAll().then(function(items){
-		response.json(items);
-	},function(e){
-		response.status(500).send();
-	});
-
-
+    db.menu.findAll().then(function(items) {
+        response.json(items);
+    }, function(e) {
+        response.status(500).send();
+    });
 });
 
-app.post('/menu', function(request, response) {
+db.menu.create(body).then(function(menu) {
+response.json(menu.toJSON());
+}, function(e) {
+console.log(e);
+});
+});
 
-    var body = _.pick(request.body, 'name', 'description', 'quantity', 'ingredients', 'category', 'cost', 'image', 'location');
-    // var body = request.body;
+
+app.get('/:id', function(request, response) {
+    var id1 = parseInt(request.params.id, 10);
+    db.menu.findById(id1).then(function(items) {
+        // var temp = JSON.stringify(items);
+        // alert(items);
+        var temp = items.image;
+        response.sendFile(__dirname + items.image);
+    }, function(e) {
+        response.status(500).send();
+    });
+});
+
+
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+        console.log("listening at port" + PORT);
+    });
+});
+
+app.get('/admin', function(request, response) {
+
+    response.sendFile(__dirname + '/public/admin.html');
+});
+
+app.post('/admin', function(request, response) {
+
+    var name = request.body.name;
+    var description = request.body.description;
+    var quantity = request.body.quantity;
+    var ingredients = request.body.ingredients;
+    var category = request.body.category;
+    var cost = request.body.cost;
+    var image = request.body.image;
+    var location = request.body.location;
+
+    var body = {
+        "name": name,
+        "description": description,
+        "quantity": quantity,
+        "ingredients": ingredients,
+        "category": category,
+        "cost": cost,
+        "image": image,
+        "location": location
+
+    }
     db.menu.create(body).then(function(menu) {
         response.json(menu.toJSON());
+        // response.send("name"+body.name);
     }, function(e) {
         console.log(e);
     });
 
-
-});
-
-db.sequelize.sync({ force: true }).then(function() {
-
-    app.listen(PORT, function() {
-
-        console.log("listening at port" + PORT);
-
-    });
 
 });
