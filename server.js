53e62febc792eb2a782db6c4ca512f1554c0d76b
Akshar Dave <akshar314@gmail.com>
2016-05-27 10:33:14 +0530

    refactor

--- server.js
+++ server.js
@@ -26,39 +26,9 @@ app.get('/all', function(request, response) {
 });
 
 
-app.get('/admin',function(request,response){
-
-    response.sendFile(__dirname+'/public/admin.html');
 
 
-});
 
-app.post('/admin', function(request, response) {
-    
-    
-    // var body = _.pick(request.body, 'name', 'description', 'quantity', 'ingredients', 'category', 'cost', 'image', 'location');
-    // var body = request.body;
-
-var name = request.body.name;
-var description = request.body.description;
-var quantity = request.body.quantity;
-var ingredients = request.body.ingredients;
-var category = request.body.category;
-var cost = request.body.cost;
-var image = request.body.image;
-var location = request.body.location;
-
-var body ={
-"name":name,
-"description":description,
-"quantity":quantity,
-"ingredients":ingredients,
-"category":category,
-"cost":cost,
-"image":image,
-"location":location
-
-}
     db.menu.create(body).then(function(menu) {
         response.json(menu.toJSON());
        // response.send("name"+body.name);
